781f3653ca73fae2f3f800fd1e7802931dc4a156
mao <1296193245@qq.com>
2022-09-07 10:49:48 +0800

    java_concurrent_programming_withdrawal_problem_without_lock_implementation

--- src/main/java/mao/t2/AccountSynchronized.java
+++ src/main/java/mao/t2/AccountSynchronized.java
@@ -1,44 +1,43 @@
-package mao.t1;
+package mao.t2;
 
 import mao.Account;
 
 /**
  * Project name(项目名称)：java并发编程_取款问题无锁实现
- * Package(包名): mao.t1
- * Class(类名): AccountUnsafe
+ * Package(包名): mao.t2
+ * Class(类名): AccountSynchronized
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2022/9/7
- * Time(创建时间)： 10:29
+ * Time(创建时间)： 10:46
  * Version(版本): 1.0
  * Description(描述)： 无
  */
 
 
-public class AccountUnsafe implements Account
+public class AccountSynchronized implements Account
 {
     private Integer balance;
 
     /**
-     * Instantiates a new Account unsafe.
+     * Instantiates a new Account synchronized.
      *
      * @param balance the balance
      */
-    public AccountUnsafe(Integer balance)
+    public AccountSynchronized(Integer balance)
     {
         this.balance = balance;
     }
 
-
     @Override
-    public Integer getBalance()
+    public synchronized Integer getBalance()
     {
         return balance;
     }
 
     @Override
-    public void withdraw(Integer amount)
+    public synchronized void withdraw(Integer amount)
     {
         balance -= amount;
     }
---
+++ src/main/java/mao/t2/Test.java
@@ -0,0 +1,24 @@
+package mao.t2;
+
+import mao.Account;
+
+/**
+ * Project name(项目名称)：java并发编程_取款问题无锁实现
+ * Package(包名): mao.t2
+ * Class(类名): Test
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/9/7
+ * Time(创建时间)： 10:47
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Account.start(new AccountSynchronized(10000));
+    }
+}
